f6f99122b1aeb88a3cfbb1a8b5d94a7386a72e75
Cristina Morocho <cmorocho718@gmail.com>
2026-05-07 04:47:40 -0400

    tweaked from metric to imperial

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function search(event) {
   let city = searchInputElement.value;
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayTemperature);
 }
